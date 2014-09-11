2e36bb7410b34236a631735e1f2642be5f30c8e3
Simrat Singh Chhabra <simratsinghchhabra@gmail.com>
2014-09-12 01:41:06 +0530

    added bm25 and tfidf

--- queryparser.py
+++ queryparser.py
@@ -1,30 +1,21 @@
-
-# -----------------------------------------------------------------------------
-# calc.py
-#
-# A simple calculator with variables -- all in one file.
-# -----------------------------------------------------------------------------
+import ply.lex as lex
+import ply.yacc as yacc
+import queryeval
 
 tokens = (
     'NAME',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'PLUS','MINUS','DIVIDE',
     'LPAREN','RPAREN', 'QUOTE', 
     )
 
 # Tokens
-
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
-t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
-t_EQUALS  = r'='
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_QUOTE   = r'"'
-t_NAME    = r'[a-zA-Z0-9_]  [a-zA-Z0-9_]*'
-
-mode = 1
-
+t_NAME    = r'[a-zA-Z0-9_][a-zA-Z0-9_]*'
 
 # Ignored characters
 t_ignore = " \t"
@@ -38,46 +29,29 @@ def t_error(t):
     t.lexer.skip(1)
     
 # Build the lexer
-import queryeval
-import ply.lex as lex
 lex.lex()
 
-
-
 # Parsing rules
-
 precedence = (
     ('left', 'DIVIDE'),
     ('left','PLUS'),
-    ('left', 'MINUS'),
-    ('left','TIMES'),
-    ('right','UMINUS'),
+    ('left', 'MINUS')
     )
 
-# dictionary of names
-names = { }
+names = dict()
 
-def p_statement_assign(t):
-    'statement : NAME EQUALS expression'
-    names[t[1]] = t[3]
+def p_statement_expr(t):
+    'statement : expression'
+    queryeval.getRanking(t[1])
+    t[0] = t[1]
 
 def p_statement_end(t):
     'words : NAME'
     t[0] = t[1]
 
 def p_statement_words(t):
-    'words : NAME words'
-              
+    'words : NAME words'              
     t[0] = t[1] + " " + t[2]
-    print t[0]
-
-
-
-def p_statement_expr(t):
-    'statement : expression'
-    #print(t[1])
-    queryeval.getRanking(t[1])
-    t[0] = t[1]
 
 def p_expression_phrase(t):
     'expression : QUOTE words QUOTE'
@@ -87,97 +61,49 @@ def p_expression_phrase(t):
 	res1 = queryeval.get_tfidfscore_phrase(t[2])
     elif mode==3:
 	res1 = queryeval.get_bm25score_phrase(t[2])
-	
     t[0] = res1
-    
 
 def p_expression_or(t):
     'expression : expression DIVIDE expression'
-
-    print "Evaluation OR  " 
-    
-    
+    print "Evaluation OR " 
     res1 = t[1]
-    res2 = t[3]
-    
-    res3 = {}
+    res2 = t[3]    
+    res3 = dict()
 
     for document in res1:
-        #print tup
-
         res3[document] = res1[document]
     for document in res2:
         if res1.has_key(document):
             res3[document] += res2[document]
         else:
             res3[document] = res2[document]
-
-    #print res1
-    #print res2
-    #print res3    
     t[0] = res3
-    print t[0]
-    print len(t[0]) 
 
 def p_expression_and(t):
     'expression : expression PLUS expression'
-
     print "Evaluation AND  " 
-    
-    
     res1 = t[1]
     res2 = t[3]
-    
-    res3 = {}
+    res3 = dict()
 
     for document in res1:
         if res2.has_key(document):
             res3[document] = res1[document] + res2[document]
 
-   
-    #print res2
-    #print res3    
     t[0] = res3
-    print t[0]
-    print len(t[0]) 
 
 def p_expression_not(t):
     'expression : expression MINUS expression'
-
     print "Evaluation NOT  " 
-    
-    
     res1 = t[1]
     res2 = t[3]
-    
-    res3 = {}
+    res3 = dict()
 
     for document in res1:
         if not(res2.has_key(document)):
             res3[document] = res1[document]
 
-   
-    #print res2
-    #print res3    
     t[0] = res3
-    print t[0]
-    print len(t[0]) 
-
-
-
-
-
-
-def p_expression_binop(t):
-    '''expression : expression TIMES expression'''
-    if t[2] == '+'  : print "evaluating " + str(t[1]) + " + " + str(t[3])
-    elif t[2] == '-': print "evaluating " + str(t[1]) + " - "  +str(t[3])
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
-
-def p_expression_uminus(t):
-    'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
 
 def p_expression_group(t):
     'expression : LPAREN expression RPAREN'
@@ -191,27 +117,16 @@ def p_expression_name(t):
 	t[0] =queryeval.get_tfidfscore(t[1])
     elif mode==3:
 	t[0] =queryeval.get_bm25score(t[1])
-		
-    print len(t[0])
-    
-# def p_expression_name(t):
-#     'expression : NAME's
-#     try:
-#         t[0] = names[t[1]]
-#     except LookupError:
-#         print("Undefined name '%s'" % t[1])
-#         t[0] = 0
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
-import ply.yacc as yacc
 yacc.yacc()
 
 while 1:
     try:
-	mode = int(raw_input("Enter mode 1(tf) 2(tfidf) 3(bm25)"))    
-        s = raw_input('calc > ')   # Use raw_input on Python 2
+		mode = int(raw_input("Enter mode 1(tf) 2(tfidf) 3(bm25)"))   
+        s = raw_input('query > ')   # Use raw_input on Python 2
     except EOFError:
         break
     yacc.parse(s)
