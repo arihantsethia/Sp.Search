be1933cc897d58d000553ab5b9d03a049fae7443
rahul003 <rahulhuilgol@gmail.com>
2014-09-14 02:17:49 +0530

    search res updates

--- views.py
+++ views.py
@@ -36,8 +36,19 @@ def search_results():
 	 	query_id = uuid.uuid4()
 		 	
 	data = {}
-	data['a']=12
-	data['b']=123
+	data['scoringMethod']=scoring_method
+	data['results']=[{},{}]
+	
+	data['results'][0]['url']='sadsad'
+	data['results'][0]['title']=123
+	data['results'][0]['rank']=1
+	data['results'][0]['content']="sadsda"
+	# data['results'][1]={}
+	data['results'][1]['url']='sadad'
+	data['results'][1]['title']=2313
+	data['results'][1]['rank']=2
+	data['results'][1]['content']="eqwe"
+	data['query_id']=str(query_id)
 	return json.dumps(data)
 
 @views.route('/about')
