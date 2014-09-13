6a4c11f26061fe515e8133c03b027a9f1b575a71
Simrat Singh Chhabra <simratsinghchhabra@gmail.com>
2014-09-13 22:54:22 +0530

    change resutls template and add func inv iews

--- views.py
+++ views.py
@@ -18,7 +18,26 @@ def search():
 	#log query,stemming, stopwords
 	return render_template("search_results.html")
 
+@views.route('/search_results',)
+def search_results():
+
+	query_string = request.args.get('query', '')
+	stemming = True if request.args.get('stemming','Y') == 'Y' else False
+	stopWords = True if request.args.get('stopWords','N') == 'Y' else False
+	
+	scoring_method = request.args.get('scoringMethod','tf')
+	start_rank = request.args.get('start_rank',0)
+	num_results = request.args.get('num_results',10)
+	query_id = request.args.get('query_id',None)
+
+	if query_id is None:
+		#generete new id and do whole search
+		query_id = 1
+		return 
+
+	# #log query,stemming, stopwords
+	# return render_template("search_results.html", result)
+
 @views.route('/about')
 def about():
 	return render_template("about.html")
-
