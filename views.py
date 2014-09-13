9965c7756cf540bb85ef86e4a887b4b2137524b7
rahul003 <rahulhuilgol@gmail.com>
2014-09-14 00:13:47 +0530

    search resul start

--- views.py
+++ views.py
@@ -2,6 +2,9 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack,Blueprint
 import smtplib
+import uuid
+import json
+
 #Defining the Blueprint for views.py
 views = Blueprint('views',__name__)
 
@@ -18,7 +21,28 @@ def search():
 	#log query,stemming, stopwords
 	return render_template("search_results.html")
 
+@views.route('/search_results',)
+def search_results():
+	query_string = request.args.get('query', '')
+	stemming = True if request.args.get('stemming','Y') == 'Y' else False
+	stopWords = True if request.args.get('stopWords','N') == 'Y' else False
+	scoring_method = request.args.get('scoringMethod','tf')
+	start_rank = request.args.get('start_rank',0)
+	num_results = request.args.get('num_results',10)
+	query_id = request.args.get('query_id',None)
+
+	if query_id is None:
+	 	#generete new id and do whole search
+	 	query_id = uuid.uuid4()
+		 	
+	data = {}
+	data['a']=12
+	data['b']=123
+	return json.dumps(data)
+
+	# #log query,stemming, stopwords
+	# return render_template("search_results.html", result)
+
 @views.route('/about')
 def about():
 	return render_template("about.html")
-
